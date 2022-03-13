853de932ae07fac514a3a9b142a4dbb2222cee4d
Midnight <arreiskurai@yahoo.com>
2022-03-13 01:24:11 -0800

    Activity and user decode hooks

---
+++ src/Components/Hooks/useActivities.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import Activities from '../../API/ActivitiesApi'
+import { setAll, setArchived } from '../../Redux/Slices/activitySlice';
+
+
+/** Gets the activities and archived activities into Redux for the logged in user */
+const useActivities = () => {
+  
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        getActivities();
+        getArchivedActivities();
+    },[])
+
+    const getActivities=async()=>{
+        try{
+          let activities = await Activities.get()
+          dispatch(setAll(activities));
+        }catch(ex){
+          console.log(ex)
+        }
+    }
+
+    const getArchivedActivities=async()=>{
+        try{
+            let archived = await Activities.getArchived()
+            dispatch(setArchived(archived))
+        }catch(ex){
+            console.log(ex)
+        }
+    }
+
+}
+
+export default useActivities
---
+++ src/Components/Hooks/useDecode.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import jwtDecode from 'jwt-decode';
+
+/** Hook to return the logged in User's Id */
+const useDecode = () => {
+
+    const [ user, setUser ]=useState();
+    const jwt = localStorage.getItem('token');
+
+    useEffect(()=>{
+        decodeToken();
+    },[jwt]);
+
+    const decodeToken=()=>{
+        try{
+            const user = jwtDecode(jwt);
+            setUser(user);
+        }catch(ex){
+            console.log(ex)
+        }
+    }
+    
+    if(user){
+        return user.user_id
+    }
+}
+
+export default useDecode
