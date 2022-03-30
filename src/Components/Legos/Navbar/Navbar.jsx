526a13655d8dcab7113fcde011484f4c9feae651
Thomas Buscemi <ThomasBuscemi124@gmail.com>
2022-03-30 03:18:42 -0400

    Got the nav link to be tabs and adjusted the sizes and imported it to another page to help with nav the site

--- src/Components/Legos/Navbar/Navbar.jsx
+++ src/Components/Legos/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react' 
+import { NavLink } from 'react-router-dom'
 import './Style/Navbar.css'
 
 
@@ -6,9 +7,15 @@ export const Navbar = () => {
   return (
     <div className='navbar-wrapper'>
         <div className='navbar-cont'>
-            <div className='nav-logo-cont'> <h1>LOGO</h1></div>
-            <div className='nav-links-cont'><h3>Links</h3></div>
-            <div className='nav-log-reg-cont'><h5>login/reg</h5></div>
+            <div className='nav-logo-cont'> <h1>Actiscribe</h1></div>
+            <div className='nav-links-cont'>
+                <NavLink className='text-links link-space' to="/">Home</NavLink>
+                <NavLink className='text-links link-space' to="/TEST">Test</NavLink>
+                <NavLink className='text-links' to="/">home</NavLink>
+                <NavLink className='text-links' to="/">login</NavLink>
+                <NavLink className='text-links' to="/">register</NavLink>
+            </div>
+            {/* <div className='nav-log-reg-cont'><h5>login/reg</h5></div> */}
         </div>
     </div>
   )
--- src/Pages/AnonHome/AnonHome.jsx
+++ src/Pages/AnonHome/AnonHome.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Navbar } from '../../Components/Legos/Navbar/Navbar'
 
 import Login from '../../Components/User/Login'
 
 export const AnonHome = () => {
   return (
     <div>
+      <Navbar/>
       AnonHome
       <br />
       <Login />
